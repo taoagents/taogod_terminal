11e44f00e14bf0533734d3898ec961becf1ea94c
taoagents <taogod@proton.me>
2025-02-26 23:51:52 -0300

    Mock envars

--- tests/test_tweet_bot.py
+++ tests/test_tweet_bot.py
@@ -1,9 +1,27 @@
+import os
 import time
 from pathlib import Path
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import pytest
 
 from taogod_terminal.tweet_bot import parse_args, TWEET_FREQ
 
+
+@pytest.fixture(autouse=True, scope="session")
+def mock_env():
+    mock_envars = {
+        "TWITTER_API_KEY": "mock_value",
+        "TWITTER_API_SECRET": "mock_value",
+        "TWITTER_API_BEARER_TOKEN": "mock_value",
+        "TWITTER_OAUTH1_ACCESS_TOKEN": "mock_value",
+        "TWITTER_OAUTH1_ACCESS_TOKEN_SECRET": "mock_value",
+        "DISCORD_USER_TOKEN": "mock_value",
+        "DISCORD_USER_ID": "mock_value",
+        "ANTHROPIC_API_KEY": "mock_value",
+    }
+    os.environ.update(mock_envars)
 
 def test_parse_args():
     with patch("argparse.ArgumentParser.parse_args", return_value=MagicMock(tweets=Path("tweets.json"))):
